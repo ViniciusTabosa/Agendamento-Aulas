7b85abbb79cf4e38e27230fc9369af017f66f6dd
Vinicius Tabosa <94081542+ViniciusTabosa@users.noreply.github.com>
2024-10-30 08:52:14 -0300

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,8 +6,8 @@ const sendEmail = async (options) => {
     port: 465, 
     secure: true,
     auth: {
-      user: 'testanildo001@gmail.com', // Seu email
-      pass: 'atwyatqopydaifew' // Senha de aplicativo gerada
+      user: '<email>', // Seu email
+      pass: '<senha gerada>' // Senha de aplicativo gerada
     },
     tls: {
       rejectUnauthorized: false // Ignora erros relacionados ao certificado
@@ -15,7 +15,7 @@ const sendEmail = async (options) => {
   });
 
   const mailOptions = {
-    from: 'testanildo001@gmail.com',
+    from: '<email>',
     to: options.email, // E-mail do destinatário
     subject: options.subject, // Assunto do email
     text: options.message // Mensagem do email
